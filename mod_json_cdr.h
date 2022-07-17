a22c3fd78c6219adbd52c4c2903f7f05f3fb33e5
kevinnguyenai <kevin.nguyen.ai@gmail.com>
2022-07-17 13:28:11 +0700

    add file license

--- mod_json_cdr.h
+++ mod_json_cdr.h
@@ -1,3 +1,33 @@
+/*
+ * FreeSWITCH Modular Media Switching Software Library / Soft-Switch Application
+ * Copyright (C) 2005-2014, Anthony Minessale II <anthm@freeswitch.org>
+ *
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is FreeSWITCH Modular Media Switching Software Library / Soft-Switch Application
+ *
+ * The Initial Developer of the Original Code is
+ * Anthony Minessale II <anthm@freeswitch.org>
+ * Portions created by the Initial Developer are Copyright (C)
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Kevin Nguyen <kevin.nguyen.ai@gmail.com>
+ *
+ * mod_json_cdr.h -- JSON CDR Module to files or curl or kafka
+ *
+ */
 #ifndef MOD_JSON_CDR_H
 #define MOD_JSON_CDR_H
 
